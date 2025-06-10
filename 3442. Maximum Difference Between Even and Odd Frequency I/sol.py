ee7a2ca49689b2ec79a4da4b4879254c93935a34
salsabila <salsabila@salsabila.dell>
2025-06-10 21:24:34 +0100

    added 3442. Maximum Difference Between Even and Odd Frequency I

---
+++ 3442. Maximum Difference Between Even and Odd Frequency I/sol.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        freq = Counter(s)
+        odd_freqs = [count for count in freq.values() if count % 2 == 1]
+        even_freqs = [count for count in freq.values() if count % 2 == 0]
+
+        if not odd_freqs or not even_freqs:
+            return 0  
+
+        return max(odd_freqs) - min(even_freqs)
